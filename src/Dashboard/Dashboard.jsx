a919385bc864a103cb749a244c8f04dbbed00d32
Christine Grimadeau <cgrimad1@yahoo.com>
2026-07-05 21:28:18 -0400

    continuing to add functions

--- src/Dashboard/Dashboard.jsx
+++ src/Dashboard/Dashboard.jsx
@@ -27,13 +27,33 @@ export default function Dashboard() {
                 setLoading(false);
             }
         };
-        
+
         fetchLiveEvents();
     }, [CLIENT_ID]);
 
-    
+// . ݁₊ ⊹ Dynamically filtering the results based on user types/interacts ⊹ . ݁˖ . ݁
+const fileredEvents = events.filter(event => {
+    // . ݁₊ ⊹ Search Bar checks matching the text against Title or the Venue Name ⊹ . ݁˖ . ݁
+    const matchesSearch = event.title.toLowerCase().includes(searchQuery.toLowerCase()) || 
+            event.venue.name.toLowerCase().includes(searchQuery.toLowerCase());
 
+    // . ݁₊ ⊹ Category Filter to check different attributes (event types) ⊹ . ݁˖ . ݁
+    const matchesCategory = selectedtype === 'All' || event.type === selectedtype;
 
+    return matchesSearch && matchesCategory;
+});
+
+// 3 Unique Summary Stats calculated using the dynamically filtered array
+const totalCount = fileredEvents.length;
+
+const averagePopularity = totalCount > 0
+? (filteredEvents.reduce((sum, item) => sum + item.averagePopularity || 0, 0) / totalCount) * 100
+: 0;
+// . ݁₊ ⊹ Count of events in New York State ⊹ . ݁˖ . ݁
+const eventsInNY = filteredEvents.filter(item => item.venue.state === 'NY').length;
+
+if (loading) {
+    return <div className="loading">Loading event data...</div>;
 }
 
-
+}

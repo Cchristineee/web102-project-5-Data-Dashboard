95fd93a046057082d75632e864c65bc61a03e7c1
Christine Grimadeau <cgrimad1@yahoo.com>
2026-07-14 18:21:47 -0400

    Imported React Router for data viz

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import App from './App.jsx'
 import './index.css'
 import Dashboard from './Dashboard/Dashboard.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Dashboard />
-  </StrictMode>,
+// ݁₊ ⊹ I'm just going to wrap my application so that it can handle switching between 
+// the main dashboard view and event details ⊹ . ݁˖ . ݁
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Dashboard />} />
+          <Route path="event/:id" element={<EventDetail />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
 )
